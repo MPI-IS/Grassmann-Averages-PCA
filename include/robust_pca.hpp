baa396cfeadc0afa03f2e078b38b2666968a0883
renficiaud <renficiaud@3149698f-976c-419d-aa71-eec1decff2c2>
2014-01-22 17:02:33 +0000

    api + doc adding a test directory
    git-svn-id: https://svn.tuebingen.mpg.de/renficiaud/robust_pca/trunk@5 3149698f-976c-419d-aa71-eec1decff2c2

--- include/robust_pca.hpp
+++ include/robust_pca.hpp
@@ -4,17 +4,45 @@
 //Released under the boost licence v1.0
 
 /*!@file
- * Robust PCA functions, following the paper of Soren 
+ * Robust PCA functions, following the paper of Soren Hausberg
+ * @todo add proper licence
  */
 
 namespace robust_pca
 {
 
+
+  /*!@brief Robust PCA subspace algorithm
+   *
+   * This class implements the robust PCA using the Grassmanian averaging.
+   * @todo add reference
+   * @author Soren Hausberg, Raffi Enficiaud
+   */
   template <class data_t>
   struct robust_pca_impl
   {
+    robust_pca_impl(){}
 
 
+
+    /*! Performs the computation of the current subspace on the elements given by the two iterators
+     */
+    template <class it_t>
+    bool batch_process(it_t it, it_t const ite)
+    {
+      return true;
+    }
+
+    /*!@brief Performs the projection of the data on the space orthogonal to the current subspace
+     *
+     * After having found the current subspace, all the data should be projected onto the orthogonal subspace
+     * of the current state.
+     */
+    template <class it_t>
+    bool project_subspace(it_t it, it_t const ite) const
+    {
+      return true;
+    }
   };
 
 }
