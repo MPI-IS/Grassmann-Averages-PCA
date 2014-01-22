47a1f28bf8a8ef6ff7448dbc1504c8deea0d02ce
renficiaud <renficiaud@3149698f-976c-419d-aa71-eec1decff2c2>
2014-01-22 16:43:38 +0000

    robust pca
    git-svn-id: https://svn.tuebingen.mpg.de/renficiaud/robust_pca/trunk@3 3149698f-976c-419d-aa71-eec1decff2c2

---
+++ include/robust_pca.hpp
@@ -0,0 +1,22 @@
+#ifndef ROBUST_PCA_HPP__
+#define ROBUST_PCA_HPP__
+
+//Released under the boost licence v1.0
+
+/*!@file
+ * Robust PCA functions, following the paper of Soren 
+ */
+
+namespace robust_pca
+{
+
+  template <class data_t>
+  struct robust_pca_impl
+  {
+
+
+  };
+
+}
+
+#endif /* ROBUST_PCA_HPP__ */
