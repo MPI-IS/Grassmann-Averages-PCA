63c363b1c72bf63b89df9a914f1753046967a5dc
renficiaud <renficiaud@3149698f-976c-419d-aa71-eec1decff2c2>
2014-01-23 17:20:57 +0000

    advance
    git-svn-id: https://svn.tuebingen.mpg.de/renficiaud/robust_pca/trunk@7 3149698f-976c-419d-aa71-eec1decff2c2

---
+++ test/test_client_function.cpp
@@ -0,0 +1,22 @@
+#include <boost/test/unit_test.hpp>
+
+#include <include/robust_pca.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+
+
+BOOST_AUTO_TEST_CASE(instance_test)
+{
+  using namespace robust_pca;
+
+  typedef boost::numeric::ublas::matrix<double> matrix_t;
+  typedef robust_pca_impl<double> robust_pca_t;
+  
+
+  robust_pca_t instance;
+
+
+  // creating some data, 1000 lines of a vector of length 5
+  matrix_t mat_data(1000, 5, 0);
+
+  BOOST_CHECK(instance.batch_process(mat_data.begin1(), mat_data.end1()));
+}
