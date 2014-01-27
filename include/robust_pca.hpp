eb665953178260f041102a2ac35ce5801414b7a0
renficiaud <renficiaud@3149698f-976c-419d-aa71-eec1decff2c2>
2014-01-27 11:17:48 +0000

    advances
    git-svn-id: https://svn.tuebingen.mpg.de/renficiaud/robust_pca/trunk@9 3149698f-976c-419d-aa71-eec1decff2c2

--- include/robust_pca.hpp
+++ include/robust_pca.hpp
@@ -6,15 +6,77 @@
 /*!@file
  * Robust PCA functions, following the paper of Soren Hausberg
  * @todo add proper licence
+
+ *
+ * @note These implementations assume the existence of boost somewhere. Currently, it is just used for 
+ * computing the norms of differences.
  */
+
+
+
+#include <boost/numeric/ublas/vector_expression.hpp>
 
 namespace robust_pca
 {
+ 
+  //! Wrapper object for infinity/max norm
+  struct norm_inf
+  {
+    template <class vector_t>
+    double operator()(vector_t const& v) const
+    {
+      return boost::numeric::ublas::norm_inf(v);
+    }
+  };
+
+
+  //! Returns the square of the @f$\ell_2@f$ norm.
+  struct norm_ell2_square
+  {
+    template <class vector_t>
+    double operator()(vector_t const& v) const
+    {
+      double acc(0);
+      for(typename vector_t::const_iterator it(v.begin()), ite(v.end());
+          it < ite;
+          ++it)
+      {
+        acc += (*it) * (*it);
+      }
+      return acc;
+    }
+  };
+
+
+  /* @brief Checks the convergence of a numerical scheme.
+   *
+   * @tparam data_t should be default constructible and constructible with one parameter.
+   *
+   * The convergence is assumed when the norm between two subsequent states is less than a certain @f$epsilon@f$.
+   */
+  template <class data_t, class norm_t = norm_inf>
+  struct convergence_check
+  {
+    const double epsilon;
+    norm_t norm_comparaison;
+    data_t previous_state;
+    convergence_check(double epsilon_) : 
+      epsilon(epsilon_), 
+      previous_state(std::numeric_limits<double>::max())
+    {}
+
+    bool operator()(data_t const& current_state)
+    {
+      return norm_comparaison(current_state - previous_state) < epsilon;
+    }
+
+  };
 
 
   /*!@brief Robust PCA subspace algorithm
    *
-   * This class implements the robust PCA using the Grassmanian averaging.
+   * This class implements the robust PCA using the Grassmanian averaging. This is the naive implementation which is
+   * suitable for small datasets. 
    * @todo add reference
    * @author Soren Hausberg, Raffi Enficiaud
    */
@@ -27,8 +89,8 @@ namespace robust_pca
 
     /*! Performs the computation of the current subspace on the elements given by the two iterators
      */
-    template <class it_t>
-    bool batch_process(it_t it, it_t const ite)
+    template <class it_t, class it_norm_t>
+    bool batch_process(it_t it, it_t const ite, it_norm_t it_norm_out)
     {
       return true;
     }
@@ -39,7 +101,7 @@ namespace robust_pca
      * of the current state.
      */
     template <class it_t>
-    bool project_subspace(it_t it, it_t const ite) const
+    bool projection_onto_subspace(it_t it, it_t const ite) const
     {
       return true;
     }
--- test/test_client_function.cpp
+++ test/test_client_function.cpp
@@ -1,29 +1,56 @@
 #include <boost/test/unit_test.hpp>
 
 #include <include/robust_pca.hpp>
+
+// data stored into a matrix
 #include <boost/numeric/ublas/matrix.hpp>
 
+// generating data randomly
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/uniform_real_distribution.hpp>
+
+
+boost::random::mt19937 rng;
 
 BOOST_AUTO_TEST_CASE(instance_test)
 {
   using namespace robust_pca;
+  using namespace boost::numeric::ublas;
+  using namespace boost::random;
 
-  typedef boost::numeric::ublas::matrix<double> matrix_t;
+  uniform_real_distribution<double> dist(-1000, 1000);
+  
+
+  typedef matrix<double> matrix_t;
   typedef robust_pca_impl<double> robust_pca_t;
   
 
   robust_pca_t instance;
 
+  const int nb_elements = 1000;
+  const int dimensions  = 5;
 
   // creating some data, 1000 lines of a vector of length 5
-  matrix_t mat_data(1000, 5, 0);
+  matrix_t mat_data(nb_elements, dimensions, 0);
 
-  BOOST_CHECK(instance.batch_process(mat_data.begin1(), mat_data.end1()));
+  matrix_t::iterator1 it(mat_data.begin1());
+  for(int i = 0; i < nb_elements; i++)
+  {
+    for(int j = 0; j < dimensions; j++, ++it)
+    {
+      *it = dist(rng);
+    }
+  }
+
+
+  std::vector<double> norms(nb_elements);
+
+  BOOST_CHECK(instance.batch_process(mat_data.begin1(), mat_data.end1(), norms.begin()));
 }
 
 
 
 boost::unit_test::test_suite* init_unit_test_suite( int argc, char* argv[] )
 {
-
+  return 0;
 }
