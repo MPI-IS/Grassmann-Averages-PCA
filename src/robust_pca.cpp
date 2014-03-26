a12bb6fe1b76353d83623d56b5d15c91c871ff8e
renficiaud <renficiaud@3149698f-976c-419d-aa71-eec1decff2c2>
2014-03-26 22:08:45 +0000

    ras
    git-svn-id: https://svn.tuebingen.mpg.de/renficiaud/robust_pca/trunk@84 3149698f-976c-419d-aa71-eec1decff2c2

--- src/robust_pca.cpp
+++ src/robust_pca.cpp
@@ -1,5 +1,5 @@
 // Copyright 2014, Max Planck Institute for Intelligent Systems.
 // Distributed under the BSD 3-Clause license.
 // (See accompanying file LICENSE.txt or copy at
-// http://opensource.org/licenses/BSD-3-Clause)
+// http://opensource.org/licenses/BSD-3-Clause )
 
