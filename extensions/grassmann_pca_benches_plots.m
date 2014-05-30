74f3c19a563e8f96fc6d63d93889dfba7b366cee
renficiaud <renficiaud@3149698f-976c-419d-aa71-eec1decff2c2>
2014-05-30 14:58:28 +0000

    RP-69
    Bench file
    
    git-svn-id: https://svn.tuebingen.mpg.de/renficiaud/robust_pca/branches/RP-47@238 3149698f-976c-419d-aa71-eec1decff2c2

--- extensions/grassmann_pca_benches_plots.m
+++ extensions/grassmann_pca_benches_plots.m
@@ -1,6 +1,8 @@
 addpath('D:\Code\robust_pca\extensions\')
 addpath('D:\Code\robust_pca\build\Release\')
 
+redo_all = true;
+
 N = 100000;
 NBSteps= 10;
 DIM = 10;
@@ -10,15 +12,22 @@ max_threads = 8;
 
 mean_mex_output = zeros(max_threads+1, NBSteps);
 
-for nb_threads = 1:max_threads
-  do_matlab = nb_threads == 1;
-  [mean_mex, var_mex, mean_matlab, var_matlab] = grassmann_pca_benches(DIM, 5, 10, nb_threads, do_matlab);
-  mean_mex_output(nb_threads+1, :) = mean_mex;
-  if do_matlab
-    mean_mex_output(1, :) = mean_matlab;
-  end %if 
-end % for
+if redo_all
+  for nb_threads = 1:max_threads
+    do_matlab = nb_threads == 1;
+    [mean_mex, var_mex, mean_matlab, var_matlab] = grassmann_pca_benches(DIM, 5, 10, nb_threads, do_matlab);
+    mean_mex_output(nb_threads+1, :) = mean_mex;
+    if do_matlab
+      mean_mex_output(1, :) = mean_matlab;
+    end %if 
+  end % for
 
+  save('benches.mat');
+else
+  S = load('benches.mat');
+  mean_mex_output = S.mean_mex_output; 
+end; %
+  
 XX = (1:NBSteps)*N/NBSteps;
 XX = repmat(XX, max_threads+1, 1);
 h = plot(XX(1,:), mean_mex_output(1, :));
