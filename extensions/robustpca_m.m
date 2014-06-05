a4c182cd950d1952a95349bdeefb4582e4868bc7
renficiaud <renficiaud@3149698f-976c-419d-aa71-eec1decff2c2>
2014-06-05 13:35:04 +0000

    RP-47
    Matlab documentation
    
    git-svn-id: https://svn.tuebingen.mpg.de/renficiaud/robust_pca/branches/RP-47@260 3149698f-976c-419d-aa71-eec1decff2c2

--- extensions/robustpca_m.m
+++ extensions/robustpca_m.m
@@ -1,6 +1,8 @@
 % Grassmann Averages for robust PCA computation.
-% ROBUSTPCA_M returns the eigenvectors of a dataset computed using the Grassmann average technique. This technique performs better
-% scalability against the number of samples and the dimension of the dataset. 
+% 
+% ROBUSTPCA_M returns the eigenvectors of a dataset computed using the Grassmann average technique. 
+% This technique performs better scalability against the number of samples and the dimension of the 
+% dataset. 
 %
 % For more information about the technique, see the <a href="matlab:web('http://ps.is.tuebingen.mpg.de/project/Robust_PCA')">Robust PCA</a> page.
 % The general forms of the function are the following:
@@ -9,17 +11,17 @@
 %   eigenvectors = ROBUSTPCA_M(DATA, trimming_percentage)
 %   eigenvectors = ROBUSTPCA_M(DATA, trimming_percentage, robustpca_configuration)
 %
-% DATA is always an N x D matrix, where N is the number of samples and D is the dimension of the problem. 
+% DATA is always an N x D matrix, where N is the number of samples and D is the dimension of the data. 
 % 
-% The first call computes the regular algorithm for all dimensions, the returned eigenvector is a 
-% D x D matrix where each column is an eigenvector. 
+% * The first call computes the regular algorithm for all dimensions, the returned eigenvector is a 
+%   D x D matrix where each column is an eigenvector. 
 %
-% The second forms trims the data by the provided percentage during the subspace computation 
-% (0 yields to the regular algorithm). 
-% trimming_percentage should be in the range [0, 100].
+% * The second forms trims the data by the provided percentage during the subspace computation 
+%   (0 yields to the regular algorithm). 
+%   trimming_percentage should be in the range [0, 100].
 %
-% The third version allows to have more control on the algorithm by providing
-% a structure. The optional fields on this structure are the following:
+% * The third version allows to have more control on the algorithm by providing
+%   a structure. The optional fields on this structure are the following:
 %
 % Optional fields
 % ---------------
@@ -27,6 +29,9 @@
 % - "max_dimensions": indicates that the max_dimensions eigenvectors should be computed. 
 %   Defaults to D. 0 < max_dimensions <= D.
 %
+% - "nb_iterations_max": indicates the maximum number of iterations for each eigenvector that should
+%   be performed. Defaults to 1000.
+% 
 % - "nb_processing_threads": indicates the number of threads used for processing.
 %
 % - "max_chunk_size": indicates the maximum amount of data processed by each of the threads. 
