cd36b431264ad1836a4e59015c55a7e468c0ae4e
renficiaud <renficiaud@3149698f-976c-419d-aa71-eec1decff2c2>
2014-06-05 13:02:56 +0000

    RP-47
    Matlab documentation
    
    git-svn-id: https://svn.tuebingen.mpg.de/renficiaud/robust_pca/branches/RP-47@256 3149698f-976c-419d-aa71-eec1decff2c2

--- extensions/robustpca_m.m
+++ extensions/robustpca_m.m
@@ -3,9 +3,29 @@
 % scalability against the number of points and the dimension of the problem. 
 %
 % For more information about the technique, see the <a href="matlab:web('http://ps.is.tuebingen.mpg.de/project/Robust_PCA')">Robust PCA</a> page.
-% 
+% The general form of the function is the following:
 %
-%   eigen_vectors = ROBUSTPCA_M(X, 0, additionnal_options)
+%   eigenvectors = ROBUSTPCA_M(data)
+%   eigenvectors = ROBUSTPCA_M(data, trimming_percentage)
+%   eigenvectors = ROBUSTPCA_M(data, trimming_percentage, robustpca_configuration)
+%
+% * Example of use on a D = 100 dimensions sample of N = 100000 random Gaussian variables:
+%   * D = 100;
+%   N = 100000;
+%   tmp = rand(D); 
+%   sigma = tmp * tmp'; 
+%   X = mvnrnd(zeros(D, 1), sigma, N);
+%   rpca_eigenvectors = ROBUSTPCA_M(X);
+% 
+% * Trimming K = 5 percent of the distribution:
+%   rpca_eigenvectors = ROBUSTPCA_M(X, 5)
+%
+% * Getting only the first 3 eigenvectors:
+%   robustpca_config = {};
+%   robustpca_config.max_dimensions = 3;
+%   rpca_eigenvectors = ROBUSTPCA_M(X, 0, robustpca_config);
+%
+%% BLABLABLA
 %
 %  See also the <a href="matlab:web('http://ps.is.tuebingen.mpg.de/project/Robust_PCA')">Robust PCA</a> page
-function eigen_vectors = robustpca_m(X, trimming, additionnal_options)
+function eigen_vectors = robustpca_m(X, trimming_percentage, additionnal_options)
