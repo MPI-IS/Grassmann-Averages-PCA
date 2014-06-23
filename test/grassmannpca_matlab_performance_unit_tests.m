be963c11fce8b4534b3aab02ba115690db4ac9af
renficiaud <renficiaud@3149698f-976c-419d-aa71-eec1decff2c2>
2014-06-23 19:23:23 +0000

    RP-35
    Fix again
    
    git-svn-id: https://svn.tuebingen.mpg.de/renficiaud/robust_pca/trunk@334 3149698f-976c-419d-aa71-eec1decff2c2

--- test/grassmannpca_matlab_performance_unit_tests.m
+++ test/grassmannpca_matlab_performance_unit_tests.m
@@ -19,12 +19,12 @@ classdef grassmannpca_matlab_performance_unit_tests < matlab.unittest.TestCase
       algorithm_config.nb_processing_threads = feature('numCores');
       
       tic
-        u = GrassmannAveragesPCA(mat', 0, algorithm_config);
+        u = GrassmannAveragesPCA(mat, 0, algorithm_config);
       t1 = toc
 
       algorithm_config.nb_processing_threads = 1;
       tic
-        u = GrassmannAveragesPCA(mat', 0, algorithm_config);
+        u = GrassmannAveragesPCA(mat, 0, algorithm_config);
       t2 = toc
       
       
