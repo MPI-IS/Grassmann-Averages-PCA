8ad82c0a6ca16af316f1f7bbc997c998a52e0bf5
renficiaud <renficiaud@3149698f-976c-419d-aa71-eec1decff2c2>
2015-01-12 14:38:45 +0000

    working on the optimisation of the computations
    git-svn-id: https://svn.tuebingen.mpg.de/renficiaud/robust_pca/branches/RP-80@398 3149698f-976c-419d-aa71-eec1decff2c2

--- test/test_mus_proximity.cpp
+++ test/test_mus_proximity.cpp
@@ -19,6 +19,22 @@
 
 #include <set>
 
+template <class matrix_t>
+void print_matrix(const matrix_t& mat)
+{
+  std::cout.precision(std::numeric_limits<double>::digits10);
+  for(size_t i = 0; i < mat.size1(); i++)
+  {
+    for(size_t j = 0; j < mat.size2(); j++)
+    {
+      std::cout << std::fixed << mat(i, j) << " ";
+    }
+    std::cout << std::endl;
+  }
+
+}
+
+
 /*!brief Handles the changes between two consecutive estimations of mus.
  *
  */
@@ -29,7 +45,7 @@ struct mus_proximity
   {
     size_t count;         //!< the number of vectors referencing the mu
     size_t index_matrix;  //!< the index of the mu in the matrix
-    size_t index_list;    //!< the index of the mu in the list
+    size_t iteration_index;    //!< the index of the mu in the list @todo check if relevant
     data_t mu;            //!< the data
     
     mu_reference(const data_t& mu_) : mu(mu_) {}
@@ -40,6 +56,10 @@ struct mus_proximity
   // we need to keep the mus in order to compute the inner product
   typedef std::list<mu_reference> mu_container_t;
   mu_container_t mus;
+  
+  // This will map the index of the algorithm to the index of the list above
+  typedef std::map<size_t, size_t> map_index_to_list_index_t;
+  map_index_to_list_index_t mapping_indices;
   
   typedef boost::numeric::ublas::symmetric_matrix<double, boost::numeric::ublas::lower> mu_angles_t;
   mu_angles_t mu_angles;
@@ -69,7 +89,27 @@ struct mus_proximity
     
     if(norm_l < 1E-10 || norm_r < 1E-10)
       return acos(0); // pi/2
+    std::cout << " -- before acos " << out / (sqrt(norm_l) * sqrt(norm_r)) << std::endl;
+    std::cout << " -- bigger than one? " << (out / (sqrt(norm_l) * sqrt(norm_r)) > 1 ? "yes" : "no") << std::endl;
     return acos(out / (sqrt(norm_l) * sqrt(norm_r)));
+  }
+  
+  // This version supposes the vectors are already normalized
+  // and the corresponding computations are simplified compared to @ref angle
+  double angle_normalized_vectors(const data_t& left, const data_t& right) const
+  {
+    typedef typename data_t::value_type scalar_t;
+    double out(0);
+    
+    scalar_t const *const left_p = &left.data()[0];
+    scalar_t const *const right_p= &right.data()[0];
+    for(size_t i = 0, j = left.size(); i < j; i++)
+    {
+      out += left_p[i] * right_p[i];
+    }
+    std::cout << " -- before acos " << out << std::endl;
+    std::cout << " -- bigger than one? " << (out > 1 ? "yes" : "no") << std::endl;
+    return acos(out);  
   }
   
   //! Removes any unnecessary element from the list of mus
@@ -134,65 +174,108 @@ struct mus_proximity
   {
     return mus;
   }
+  
+  //! Updates the count of a particular mu
+  //!
+  //! @param[in] delta_count positive count means added reference, while negative count means
+  //!            removed reference
+  void update_count(size_t iteration_index, int delta_count)
+  {
+    assert(mapping_indices.count(iteration_index) > 0);
+    size_t index_in_list = mapping_indices[iteration_index];
+    
+    typename mu_container_t::iterator it(mus.begin());
+    std::advance(it, index_in_list);
+    it->count += delta_count;
+  }
 
 
-  void add_mu(const data_t& new_mu, size_t nb_references)
+  //! Adds a mu to the managed list of mus
+  //!
+  //! The mu is stored in the first available place
+  mu_reference const& add_mu(const data_t& new_mu, size_t iteration_index, size_t nb_references)
   {
     using namespace std; // bringing acos
     
-    mus.push_back(mu_reference(new_mu));
-    mu_reference &last = mus.back();
-    last.index_matrix = mu_angles.size1();
-    last.index_list   = mus.size()-1;
-    last.count = nb_references;
+    // finding the first available place to put this new mu:
+    size_t index_in_list(0);
+    typename mu_container_t::iterator it(mus.begin());
+    typename mu_container_t::iterator ite(mus.end());
+    
+    for(; it != ite; ++it, index_in_list++)
+    {
+      if(!it->count)
+      {
+        break;
+      }
+    }
+    
+    if(it != ite)
+    {
+      it->mu = new_mu;
+      it->count= nb_references;
+      it->iteration_index = iteration_index;
+      // the position in the matrix stays the same
+    }
+    else
+    {
+      // there is nothing to prune here anyway, so we add a new column/row to the 
+      // matrix
+      
+      mus.push_back(mu_reference(new_mu));
+      it = --mus.end();
+      it->index_matrix = mu_angles.size1();
+      it->iteration_index   = iteration_index;
+      index_in_list    = mus.size()-1;
+      it->count = nb_references;      
+      
+      // adding a row / column
+      std::cout << "before" << std::endl;
+      print_matrix(this->get_angle_matrix());
+      mu_angles.resize(mu_angles.size1() + 1, true);
+      mu_angles(mu_angles.size1()-1, mu_angles.size1()-1) = 0;
+      std::cout << "after" << std::endl;
+      print_matrix(this->get_angle_matrix());
+            
+    }
+    
+    mapping_indices[iteration_index] = index_in_list;
     
     
-    // here we can test if there is something to prune, if so, replace the 
-    // first available column by this vector and do not forget to remove it from
-    // the list
-    
-    
-    // adding a row / column
-    mu_angles.resize(mu_angles.size1() + 1, true);
-    mu_angles(mu_angles.size1()-1, mu_angles.size1()-1) = 0;
-    
-    typename mu_container_t::iterator it(mus.begin()), ite(mus.end());
-    --ite;
-    for(; it != ite; ++it)
+    for(typename mu_container_t::const_iterator it2(mus.begin()); it != ite; ++it)
     {
       // computing the inner product and storing into the appropriate place of
       // the "distance" matrix
-      mu_angles(it->index_matrix, last.index_matrix) = angle(last.mu, it->mu);
+      // we avoid the computation of these angles for the elements that will get pruned
+    
+      if(it2 == it)
+        continue;
+        
+      if(it2->count == 0)
+        continue;
+    
+      mu_angles(it2->index_matrix, it->index_matrix) = angle(it->mu, it2->mu);
+      std::cout << " -- " << mu_angles(it2->index_matrix, it->index_matrix) << std::endl;
     }
+    
+    return *it;
   }
 
 };
 
 
-template <class matrix_t>
-void print_matrix(const matrix_t& mat)
-{
-  std::cout.precision(std::numeric_limits<double>::digits10);
-  for(size_t i = 0; i < mat.size1(); i++)
-  {
-    for(size_t j = 0; j < mat.size2(); j++)
-    {
-      std::cout << std::fixed << mat(i, j) << " ";
-    }
-    std::cout << std::endl;
-  }
 
-}
 
-BOOST_AUTO_TEST_CASE(test_prune)
+BOOST_AUTO_TEST_CASE(test_add_mu)
 {
   namespace ub = boost::numeric::ublas;
+  namespace ga = grassmann_averages_pca;
 
   typedef ub::vector<double> data_t;
   typedef mus_proximity<data_t> mu_angle_helper_t;
   mu_angle_helper_t instance;
 
-  const int nb_elements = 10;
+  const int nb_elements = 3;
   const int dimensions = 5;
 
   for(int i = 0; i < nb_elements; i++)
@@ -200,13 +283,20 @@ BOOST_AUTO_TEST_CASE(test_prune)
     data_t current(dimensions);
     for(int j = 0; j < dimensions; j++)
     {
-      current(j) = i;
+      current(j) = (i + 1);
     }
+    
+    current /= ga::details::norm2()(current);
 
-    instance.add_mu(current, (i + 3) % nb_elements); // the 3rd should be pruned
-    print_matrix(instance.get_angle_matrix());
+    instance.add_mu(current, i+1, 10);
   }
+  
+  print_matrix(instance.get_angle_matrix());
+  
+}
 
+
+#if 0
 
 
 
@@ -250,3 +340,4 @@ BOOST_AUTO_TEST_CASE(test_prune)
   }
 
 }
+#endif
