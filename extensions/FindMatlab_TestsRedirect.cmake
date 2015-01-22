82a3f2417d4ec5825a0e3a2e18367bef968a9fe8
renficiaud <renficiaud@3149698f-976c-419d-aa71-eec1decff2c2>
2015-01-22 08:42:33 +0000

    File renaming Adding an application for the regular grassmann PCA
    git-svn-id: https://svn.tuebingen.mpg.de/renficiaud/robust_pca/branches/RP-80@421 3149698f-976c-419d-aa71-eec1decff2c2

--- extensions/FindMatlab_TestsRedirect.cmake
+++ extensions/FindMatlab_TestsRedirect.cmake
@@ -1,3 +1,17 @@
+#=============================================================================
+# Copyright 2014 Raffi Enficiaud
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distribute this file outside of CMake, substitute the full
+#  License text for the above reference.)
+
+
 # Usage: cmake 
 #   -Dtest_timeout=180 
 #   -Dworking_directory="." 
