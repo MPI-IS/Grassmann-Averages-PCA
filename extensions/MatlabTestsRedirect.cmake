4def0306096c0e8efa48f74eb2e1e7353947ee99
renficiaud <renficiaud@3149698f-976c-419d-aa71-eec1decff2c2>
2015-04-14 07:49:26 +0000

    Merged revision(s) 432-438 from robust_pca/trunk: Switching to the new version of FindMatlab ........ REDUCE_VISIBILITY does not exist anymore ........ Fixing the matlab issues ........ Fixing the matlab issues (bis) ........ Fixing the matlab issues (ter) ........ Fixing the matlab issues (4) ........ Fixing the matlab issues (5) ........
    git-svn-id: https://svn.tuebingen.mpg.de/renficiaud/robust_pca/branches/RP-80@439 3149698f-976c-419d-aa71-eec1decff2c2

--- extensions/MatlabTestsRedirect.cmake
+++ extensions/MatlabTestsRedirect.cmake
@@ -1,3 +1,20 @@
+# This is an undocumented internal helper for the FindMatlab
+# module ``matlab_add_unit_test`` command.
+
+#=============================================================================
+# Copyright 2014-2015 Raffi Enficiaud, Max Planck Society
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distribute this file outside of CMake, substitute the full
+#  License text for the above reference.)
+
+
 # Usage: cmake
 #   -Dtest_timeout=180
 #   -Dworking_directory="."
@@ -36,20 +53,29 @@ endforeach()
 
 set(unittest_to_run "runtests('${unittest_file_to_run_name}'), exit(max([ans(1,:).Failed]))")
 if(no_unittest_framework)
-  set(unittest_to_run "${unittest_file_to_run_name}, exit(0)")
+  set(unittest_to_run "try, ${unittest_file_to_run_name}, catch err, disp('An exception has been thrown during the execution'), disp(err), disp(err.stack), exit(1), end, exit(0)")
 endif()
 
 set(Matlab_SCRIPT_TO_RUN
-    "addpath(${concat_string})\; path, ${cmd_to_run_before_test}, ${unittest_to_run}"
+    "addpath(${concat_string}), path, ${cmd_to_run_before_test}, ${unittest_to_run}"
    )
 
-set(Matlab_LOG_FILE ${output_directory}/${test_name}.log)
+set(Matlab_LOG_FILE "${output_directory}/${test_name}.log")
+
+set(devnull)
+if(UNIX)
+  set(devnull INPUT_FILE /dev/null)
+elseif(WIN32)
+  set(devnull INPUT_FILE NUL)
+endif()
 
 execute_process(
-  COMMAND ${Matlab_PROGRAM} ${Matlab_UNIT_TESTS_CMD} -logfile ${Matlab_LOG_FILE} -r ${Matlab_SCRIPT_TO_RUN}
+  COMMAND "${Matlab_PROGRAM}" ${Matlab_UNIT_TESTS_CMD} -logfile "${test_name}.log" -r "${Matlab_SCRIPT_TO_RUN}"
   RESULT_VARIABLE res
   TIMEOUT ${test_timeout}
   OUTPUT_QUIET # we do not want the output twice
+  WORKING_DIRECTORY "${output_directory}"
+  ${devnull}
   )
 
 if(NOT EXISTS ${Matlab_LOG_FILE})
@@ -58,7 +84,7 @@ endif()
 
 # print the output in any case.
 file(READ ${Matlab_LOG_FILE} matlab_log_content)
-message("Matlab test ${name_of_the_test} FAILED\n${matlab_log_content}") # if we put FATAL_ERROR here, the file is indented.
+message("Matlab test ${name_of_the_test} output:\n${matlab_log_content}") # if we put FATAL_ERROR here, the file is indented.
 
 
 if(NOT (res EQUAL 0))
